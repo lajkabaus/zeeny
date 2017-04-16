8f7a3505c6d8e32df2dc149ca799cbad347d7d8e
Marko Lainović <marko.lainovic@bitgear.rs>
2017-04-16 05:29:05 +0200

    backend update
    - added basic camera callback, but it crashes on 7.0

--- app/src/main/java/com/mici/apps/zeeny/MainActivity.java
+++ app/src/main/java/com/mici/apps/zeeny/MainActivity.java
@@ -1,17 +1,55 @@
 package com.mici.apps.zeeny;
 
 import android.content.Intent;
-import android.os.Handler;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView image;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN );
         setContentView(R.layout.activity_main);
+
+        /** helpful references */
+        Button click = (Button)findViewById(R.id.sendButton);
+        image = (ImageView)findViewById(R.id.cameraView);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    /**
+     * google API
+     */
+
+    public void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            image.setImageBitmap(imageBitmap);
+        }
     }
 }
