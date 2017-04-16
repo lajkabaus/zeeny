c39515c535de2b934a81eb8bea1e293468742445
Marko Lainović <marko.lainovic@bitgear.rs>
2017-04-17 00:23:28 +0200

    backend - full size image fixed - thumbnail drawing fixed
    testing permissions mechanisms

--- app/src/main/java/com/mici/apps/zeeny/MainActivity.java
+++ app/src/main/java/com/mici/apps/zeeny/MainActivity.java
@@ -1,16 +1,22 @@
 package com.mici.apps.zeeny;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
+import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Build.VERSION;
 import android.os.Environment;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -32,16 +38,27 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity
 {
-    ImageView image;
+    ImageView _image;
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    static final int PICK_CONTACT_REQUEST = 1;
+    static final int REQUEST_PICK_CONTACT  = 2;
+
+    static final int REQUEST_WRITE_STORAGE = 3;
+    static final int REQUEST_USE_CAMERA    = 4;
 
     boolean _isPictureTaken   = false;
     boolean _isFlashAvailable = false;
     boolean _isFlashOn        = false;
 
+    boolean _hasPermission_IO_write = false;
+    boolean _hasPermission_camera   = false;
+
+
     private static CameraManager _cameraManager = null;
     private static String _cameraId = "";
+
+    private String _lastImageName = "";
+    private Uri _lastImageUri;
+    private static final String appName = "Zeeny";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -61,13 +78,14 @@ public class MainActivity extends AppCompatActivity
 
         initFlash();
 
-        image = (ImageView)findViewById(R.id.cameraView);
-        image.setOnClickListener(new View.OnClickListener()
+        _image = (ImageView)findViewById(R.id.cameraView);
+        _image.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                takeImage();
+                checkCameraPermissions();
+                createImage();
             }
         });
 
@@ -102,7 +120,7 @@ public class MainActivity extends AppCompatActivity
         Log.i("Pick contact!", "");
         Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
         pickContactIntent.setType(ContactsContract.CommonDataKinds.Email.CONTENT_TYPE); // Show user only contacts w/ phone numbers
-        startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
+        startActivityForResult(pickContactIntent, REQUEST_PICK_CONTACT);
     }
 
     /**
@@ -112,6 +130,12 @@ public class MainActivity extends AppCompatActivity
     {
         if ( false == _isPictureTaken )
         {
+            return;
+        }
+
+        if ( true == _lastImageName.isEmpty() )
+        {
+            Toast.makeText(this, "no new image", Toast.LENGTH_SHORT).show();
             return;
         }
 
@@ -128,61 +152,178 @@ public class MainActivity extends AppCompatActivity
         emailIntent.putExtra(Intent.EXTRA_CC, "");
         emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Slika grla");
         emailIntent.putExtra(Intent.EXTRA_TEXT, "Evo slike grla."+"\n\n"+"Mici");
-
-        String root = Environment.getExternalStorageDirectory().toString();
-        File myDir = new File(root);
-        String fname = "Slika grla" + ".jpg";
-        File bitmapFile = new File(myDir, fname);
-
-        Uri myUri = Uri.fromFile(bitmapFile);
-        emailIntent.putExtra(Intent.EXTRA_STREAM, myUri);
+        emailIntent.putExtra(Intent.EXTRA_STREAM, _lastImageUri);
 
         try
         {
             startActivity(Intent.createChooser(emailIntent, "Send e-mail with"));
             finish();
-            Log.i("Sending email done", "");
 
         } catch (android.content.ActivityNotFoundException ex) {
-            Toast.makeText(MainActivity.this,
-                    "There is no e-mail client installed.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "There is no e-mail client installed.", Toast.LENGTH_SHORT).show();
         }
+
+        _lastImageName = "";
+    }
+
+    /* Checks if external storage is available for read and write */
+    public static boolean isExternalStorageWritable()
+    {
+        String state = Environment.getExternalStorageState();
+        if ( Environment.MEDIA_MOUNTED.equals(state) )
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public static boolean isExternalStorageReadable()
+    {
+        String state = Environment.getExternalStorageState();
+        if  (   Environment.MEDIA_MOUNTED.equals(state)
+            ||  Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)
+            )
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public File getImagesDir()
+    {
+        // Get the directory for the user's public pictures directory.
+        File folder = new File( Environment.getExternalStoragePublicDirectory( Environment.DIRECTORY_PICTURES ), "/" + appName);
+        if ( false == folder.exists() )
+        {
+            if (false == folder.mkdir())
+            {
+                Log.e("", "Directory not created");
+                folder = null;
+            }
+        }
+        return folder;
+    }
+
+    /**
+     * load a file from app's directory
+     * @param fileName
+     * @return
+     */
+    public File loadFile(String fileName)
+    {
+        if ( false == isExternalStorageReadable() )
+        {
+            return null;
+        }
+
+        if ( true == fileName.isEmpty() )
+        {
+            return null;
+        }
+
+        File file = new File( _lastImageUri.toString() );
+        return file;
     }
 
     /**
      * save the bitmap with given a file-name
-     * @param bmp
-     * @param imageName
      * @return
      * @throws IOException
      */
-    public static File saveBitmap(Bitmap bmp, String imageName) throws IOException
+    public File createImageFile() throws IOException
     {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        bmp.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
-        String root = Environment.getExternalStorageDirectory().toString();
-        File myDir = new File(root);
-        String fname = imageName + ".jpg";
-        File file = new File(myDir, fname);
-        if (file.exists()) file.delete();
-        Log.i("LOAD", root + fname);
+        if ( false == _lastImageName.isEmpty() )
+        {
+            _lastImageName = "";
+            return null;
+        }
 
+        if ( false == isExternalStorageWritable() )
+        {
+            _lastImageName = "";
+            return null;
+        }
+
+        checkWritePermissions();
+        if ( false == _hasPermission_IO_write )
+        {
+            _lastImageName = "";
+            return null;
+        }
+
+        /** save to file using the current date and time-stamp */
+        String date = "";
         try
         {
-            FileOutputStream out = new FileOutputStream(file);
-            out.write(bytes.toByteArray());
-            out.flush();
-            out.close();
-        } catch (Exception e) {
+            date = new SimpleDateFormat("HH:mm:ss_'on'_dd.MM.yyyy").format(new Date());
+        }
+        catch ( Exception e)
+        {
             e.printStackTrace();
         }
 
-        return file;
+        if ( true == date.isEmpty() )
+        {
+            return null;
+        }
+
+        File imagesDir = getImagesDir();
+        if ( ( null == imagesDir ) || ( false == imagesDir.isDirectory() ) )
+        {
+            return null;
+        }
+
+        File image = new File(imagesDir, date.toString() + ".jpg");
+        if ( true == image.exists() )
+        {
+            image.delete();
+        }
+
+        _lastImageName = image.toString();
+        _lastImageUri = Uri.fromFile(image);
+
+        return image;
+    }
+
+    private void createImage()
+    {
+        if ( false == _hasPermission_camera )
+        {
+            Toast.makeText(this, "Camera usage not allowed", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Ensure that there's a camera activity to handle the intent
+        if ( null != takePictureIntent.resolveActivity( getPackageManager() ) )
+        {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try
+            {
+                photoFile = createImageFile();
+            }
+            catch (IOException ioe)
+            {
+                Toast.makeText(this, "File I/O Error!", Toast.LENGTH_SHORT).show();
+                ioe.printStackTrace();
+            }
+
+            // Continue only if a file was successfully created
+            if ( null != photoFile )
+            {
+                takePictureIntent.putExtra( MediaStore.EXTRA_OUTPUT, _lastImageUri );
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
     }
 
     public  void initFlash()
     {
-        if ( false == getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH) ) {
+        if ( false == getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH) )
+        {
             _isFlashAvailable = false;
             return;
         }
@@ -256,17 +397,63 @@ public class MainActivity extends AppCompatActivity
      * google API
      */
 
-    /**
-     * start the camera
-     */
-    public void takeImage()
+    public void checkWritePermissions()
     {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if ( null != takePictureIntent.resolveActivity( getPackageManager() ) )
+        _hasPermission_IO_write =
+            ( PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE ) );
+
+        if ( false == _hasPermission_IO_write )
         {
-            turnFlashON();
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            ActivityCompat.requestPermissions(  this,
+                                                new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE },
+                                                REQUEST_WRITE_STORAGE   );
         }
+    }
+
+    public void checkCameraPermissions()
+    {
+        _hasPermission_camera =
+                ( PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) );
+
+        if ( false == _hasPermission_camera )
+        {
+            ActivityCompat.requestPermissions(  this,
+                                                new String[]{ Manifest.permission.CAMERA },
+                                                REQUEST_USE_CAMERA   );
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode)
+        {
+            case REQUEST_WRITE_STORAGE:
+            {
+                if ( grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED )
+                {
+                    _hasPermission_IO_write = true;
+                }
+                else
+                {
+                    Toast.makeText(this, "permission to write to files is required", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+            case REQUEST_USE_CAMERA:
+            {
+                if ( grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED )
+                {
+                    _hasPermission_camera = true;
+                }
+                else
+                {
+                    Toast.makeText(this, "permission to use camera is required", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
+
     }
 
     /**
@@ -279,54 +466,50 @@ public class MainActivity extends AppCompatActivity
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         /** callback after taking taking the image */
-        if ( REQUEST_IMAGE_CAPTURE == requestCode )
+        switch (requestCode)
         {
-            turnFlashOFF();
-            if ( RESULT_OK == resultCode )
+            case REQUEST_IMAGE_CAPTURE:
             {
-                Bundle extras = data.getExtras();
-                Bitmap imageBitmap = (Bitmap) extras.get("data");
-                image.setImageBitmap(imageBitmap);
-
-                /** save to file */
-                try
+                if ( RESULT_OK == resultCode )
                 {
-                    /** save to file using the current date and time-stamp */
-                    String date = "";
-                    try
+                    /** set thumbnail */
+                    Bundle extras = data.getExtras();
+
+                    Bitmap imageBitmap = null;
+                    if ( null != extras )
                     {
-                        date = new SimpleDateFormat("HH:mm:ss_'on'_dd.MM.yyyy").format(new Date());
+                        imageBitmap = (Bitmap) extras.get("data");
                     }
-                    catch ( Exception e)
+                    else
                     {
-                        e.printStackTrace();
+                        if ( false == _lastImageName.isEmpty() )
+                        {
+                            imageBitmap = ThumbnailUtils.extractThumbnail(BitmapFactory.decodeFile(_lastImageName), _image.getWidth(), _image.getHeight());
+                        }
                     }
 
-                    File newfile;
-                    if ( false == date.isEmpty() ) {
-                        newfile = saveBitmap(imageBitmap, "zeeny_" + date);
-                        Toast.makeText(MainActivity.this,
-                                "Image saved with filename: " + newfile.getName().toString()
-                                , Toast.LENGTH_SHORT).show();
+                    if ( null != imageBitmap )
+                    {
+                        _image.setImageBitmap(imageBitmap);
                     }
 
-                } catch (IOException ioe) {
-                    Toast.makeText(MainActivity.this,
-                            "File I/O Error!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Image saved", Toast.LENGTH_SHORT).show();
+                    _isPictureTaken = true;
                 }
-
-                /** mark the image as ready */
-                _isPictureTaken = true;
+                break;
             }
-        }
 
-        /** callback after picking the contact */
-        else if (
-                ( PICK_CONTACT_REQUEST == requestCode )
-                        &&  ( RESULT_OK == resultCode )
-                )
-        {
-            sendImage();
+            case REQUEST_PICK_CONTACT:
+            {
+                if ( RESULT_OK == resultCode )
+                {
+                    /** send the image */
+                    sendImage();
+                }
+                break;
+            }
+
+            default: {}
         }
     }
 }
